2bf993f41ad917e46b310040106e2c7f9da70c11
Khizar <khizarce@gmail.com>
2021-06-03 15:19:56 -0400

    3b work

--- part3/index.js
+++ part3/index.js
@@ -1,7 +1,9 @@
 const { response, request } = require('express')
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.json())
 
 const requestLogger = (request, response, next) => {
@@ -95,6 +97,6 @@ const unknownEndpoint = (request, response) => {
   
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
