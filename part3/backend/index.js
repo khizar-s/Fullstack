e2bcf33f7c810d53801faf9642bab7b18ea85d43
Khizar <khizarce@gmail.com>
2021-06-22 22:23:11 -0400

    3d work

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -19,27 +19,6 @@ const requestLogger = (request, response, next) => {
   
 app.use(requestLogger)
 
-let notes = [
-    {
-      id: 1,
-      content: "HTML is easy",
-      date: "2019-05-30T17:30:31.098Z",
-      important: true
-    },
-    {
-      id: 2,
-      content: "Browser can execute only Javascript",
-      date: "2019-05-30T18:39:34.091Z",
-      important: false
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      date: "2019-05-30T19:20:14.298Z",
-      important: true
-    }
-]
-
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 })
@@ -63,7 +42,6 @@ app.get('/api/notes/:id', (request, response, next) => {
 })
 
 app.delete('/api/notes/:id', (request, response, next) => {
-
     Note.findByIdAndRemove(request.params.id)
         .then(result => {
             response.status(204).end()
@@ -71,15 +49,8 @@ app.delete('/api/notes/:id', (request, response, next) => {
         .catch(error => next(error))
 })
 
-app.post('/api/notes', (request, response) => {
-
+app.post('/api/notes', (request, response, next) => {
     const body = request.body
-    
-    if (!body.content) {
-        return response.status(400).json({
-            error: 'content missing'
-        })
-    }
 
     const note = new Note({
         content: body.content,
@@ -87,9 +58,13 @@ app.post('/api/notes', (request, response) => {
         date: new Date()
     })
 
-    note.save().then(savedNote => {
-        response.json(savedNote)
-    })
+    note
+        .save()
+        .then(savedNote => savedNote.toJSON())
+        .then(savedAndFormattedNote => {
+            response.json(savedAndFormattedNote)
+        })
+        .catch(error => next(error))
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
@@ -118,6 +93,8 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
     }
     
     next(error)
