2e99e0de9921d8e6ce75889b732e50f9712a2d72
Khizar <khizarce@gmail.com>
2021-07-14 23:41:01 -0400

    Exercises for 4d

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -4,17 +4,26 @@ const cors = require('cors')
 require('express-async-errors')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
+const userRouter = require('./controllers/users')
 const config = require('./utils/config')
 const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
 
 const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
 
 app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', userRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
---
+++ part4/bloglist/controllers/users.js
@@ -0,0 +1,26 @@
+const usersRouter = require('express').Router()
+const bcrypt = require('bcrypt')
+const User = require('../models/user')
+
+usersRouter.post('/', async (request, response) => {
+  const body = request.body
+
+  const saltRounds = 10
+  const passwordHash = await bcrypt.hash(body.password, saltRounds)
+
+  const user = new User({
+    username: body.username,
+    password: passwordHash,
+    name: body.name
+  })
+
+  const savedUser = await user.save()
+  response.status(201).json(savedUser)
+})
+
+usersRouter.get('/', async (request, response) => {
+  const users = await User.find({})
+  response.json(users)
+})
+
+module.exports = usersRouter
